981786664c994933d04b7633bfedfd8a77b819fe
Frosty-H <hanna.frosterus@student.lut.fi>
2024-01-14 19:20:42 +0200

    first commit

---
+++ src/main/java/main/App.java
@@ -0,0 +1,15 @@
+package main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+
+    }
+}
